98e7dc9e93b95d3cfaecb087496f70e22ff061f3
Nicolas Dubrulle <n.dubrulle@partner.samsung.com>
2020-03-24 11:22:22 +0100

    1st test

---
+++ python_export/test.py
@@ -0,0 +1,58 @@
+import csv
+import json
+# import requests
+
+
+# response = requests.get('https://connect.deezer.com/oauth/auth.php')
+
+# print("reponse")
+# print(response.content)
+
+
+
+# data = [["Ravi", "9", "550"], ["Joe", "8", "500"], ["Brian", "9", "520"]]
+# with open('students.csv', 'wb') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',')
+#     writer.writerows(data)
+
+
+def convert_csv_to_json(csv_path):
+
+    headers = []
+    liste = []
+    csv_version = 1 # csv version
+
+
+    with open(csv_path, 'r') as csvfile:
+        val = {}
+        spamreader = csv.reader(csvfile, delimiter=',',)
+        for row in spamreader:
+            if row[0] == 'Province/State':
+                headers = row[0: 6]
+                print (headers)
+            else:
+                # headers = row[2: 5] + row[7: 10]
+                headers = ['Province/State', 'Country/Region', 'Last Update', 'Confirmed', 'Deaths', 'Recovered']
+                print (headers)
+                csv_version = 2
+            break
+
+        for row in spamreader:
+            if csv_version == 1:
+                row = row[0: 6]              # get rid if useless info
+            if csv_version == 2:
+                row = row[2: 5] + row[7: 10] # remap to old format
+
+            for i, j in enumerate(row):
+                val[headers[i]] = j
+
+            liste.append(val)
+        print(liste)
+
+    return liste
+
+json_info = convert_csv_to_json("Z:\\Nicolas\\git\\github\\COVID-19\\csse_covid_19_data\\csse_covid_19_daily_reports\\03-22-2020.csv")
+
+
+with open('03-22-2020.json', 'w') as outfile:
+    json.dump(json_info, outfile)

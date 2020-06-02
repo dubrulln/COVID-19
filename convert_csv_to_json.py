5cf85992b3276e6b9ad068faaa6adafc1db30c8c
Nicolas Dubrulle <n.dubrulle@partner.samsung.com>
2020-06-02 08:19:24 +0200

    fix format

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -71,14 +71,14 @@ def convert_csv_to_json(csv_path):
 INPUT_DIR = Path("./csse_covid_19_data/csse_covid_19_daily_reports").absolute()
 
 today = datetime.date.today()
-delta = today - datetime.timedelta(days=10)
+delta = today - datetime.timedelta(days=7)
 for f in [f_ for f_ in listdir(str(INPUT_DIR)) if isfile(join(str(INPUT_DIR), f_))]:
     if f.endswith('.csv'):
 
-        # date = datetime.datetime.strptime(f.replace('.csv', ''), '%d-%m-%Y').date()
+        date = datetime.datetime.strptime(f.replace('.csv', ''), '%m-%d-%Y').date()
 
-        # if delta > date:
-        #     continue
+        if delta > date:
+            continue
 
         full_path = join(str(INPUT_DIR), f)
         full_new_path = join(str(INPUT_DIR), f.replace('.csv', '.json'))
